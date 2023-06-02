c48ddba9c26bae61197701fc11bbb3ff08d75531
Ace <35065632+RealSGM@users.noreply.github.com>
2023-06-02 15:26:35 +0100

    Created Android Project with Template

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.cybersecurity {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.cybersecurity to javafx.fxml;
-    exports com.example.cybersecurity;
-}
