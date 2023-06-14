cf4aa7b5a48daedda5a226e0e43b69b533d2ce24
koreck281 <116664115+koreck281@users.noreply.github.com>
2023-06-15 00:04:16 +0100

    Fixed Dark Button and Language Button

--- app/src/main/java/com/example/cybersecurityapp/MenuActivity.java
+++ app/src/main/java/com/example/cybersecurityapp/MenuActivity.java
@@ -1,14 +1,26 @@
 package com.example.cybersecurityapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.cybersecurityapp.databinding.ActivityMenuBinding;
 
+import java.util.Locale;
+
 public class MenuActivity extends AppCompatActivity {
     private ActivityMenuBinding binding;
+
+    boolean isNight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,32 +30,6 @@ public class MenuActivity extends AppCompatActivity {
         ViewPager2 viewPager2 = findViewById(R.id.menu_pager);
         MenuPagerAdapter menuPagerAdapter = new MenuPagerAdapter(this);
         viewPager2.setAdapter(menuPagerAdapter);
-
-//
-//        darkMode = findViewById(R.id.dark);
-//        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO) {
-//            isNight = false;
-//        } else {
-//            isNight = true;
-//        }
-//
-//        darkMode.setOnClickListener(v -> {
-//            if (isNight) {
-//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-//                isNight = false;
-//            } else {
-//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-//                isNight = true;
-//
-//            }
-//        });
-
-
-
-
-
-
-
 
 
 
@@ -59,5 +45,70 @@ public class MenuActivity extends AppCompatActivity {
 //        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_menu);
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 //        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    public void darkMethod(MenuItem item){
+        if (isNight) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            isNight = false;
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            isNight = true;
+
+        }
+    }
+        public void showDialog(MenuItem item) {
+
+        final String[] langs =  {"English","Francés/Francesa","Español"};
+        AlertDialog.Builder mBuilder =  new AlertDialog.Builder(MenuActivity.this);
+        mBuilder.setTitle("");
+        mBuilder.setSingleChoiceItems(langs, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+
+                if(i==0){
+                    setLocale("en");
+                    MainActivity.val = 0;
+                    Intent intent = getIntent();
+                    finish();
+                    startActivity(intent);
+                } else if (i==1)
+                {
+                    setLocale("fr");
+                    MainActivity.val = 1;
+                    Intent intent = getIntent();
+                    finish();
+                    startActivity(intent);
+                } else if (i==2)
+                {
+                    setLocale("es");
+                    MainActivity.val  = 2;
+                    Intent intent = getIntent();
+                    finish();
+                    startActivity(intent);
+                }
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog mDialog = mBuilder.create();
+        mDialog.show();
+    }
+
+    private void setLocale(String lang){
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+        getBaseContext().getResources().updateConfiguration(config,getBaseContext().getResources().getDisplayMetrics());
+
+
+        SharedPreferences.Editor editor = this.getSharedPreferences("Settings",MODE_PRIVATE).edit();
+        editor.putString("My_Language",lang);
+        editor.apply();
+    }
+
+
+    public void showDialog() {
     }
 }
--- app/src/main/java/com/example/cybersecurityapp/menufrag/MenuProfileFragment.java
+++ app/src/main/java/com/example/cybersecurityapp/menufrag/MenuProfileFragment.java
@@ -20,6 +20,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.cybersecurityapp.MainActivity;
 import com.example.cybersecurityapp.R;
@@ -75,13 +76,17 @@ public class MenuProfileFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_menu_profile, container, false);
+
+
     }
 
 
@@ -89,80 +94,12 @@ public class MenuProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        View a = getView();
-
-        darkMode = a.findViewById(R.id.dark);
-        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO) {
-            isNight = false;
-        } else {
-            isNight = true;
-        }
-
-        darkMode.setOnClickListener(v -> {
-            if (isNight) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                isNight = false;
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                isNight = true;
-
-            }
-        });
-    }
 
 
-    private void showDialog() {
-
-        final String[] langs =  {"English","francés/francesa","español"};
-        AlertDialog.Builder mBuilder =  new AlertDialog.Builder(getActivity());
-        mBuilder.setTitle("");
-        mBuilder.setSingleChoiceItems(langs, -1, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int i) {
-
-                if(i==0){
-                    setLocale("en");
-                    MainActivity.val = 0;
-//                    Intent intent = getIntent();
-//                    finish();
-//                    startActivity(intent);
-                } else if (i==1)
-                {
-                    setLocale("fr");
-                    MainActivity.val = 1;
-//                    Intent intent = getIntent();
-//                    finish();
-//                    startActivity(intent);
-                } else if (i==2)
-                {
-                    setLocale("es");
-                    MainActivity.val  = 2;
-//                    Intent intent = getIntent();
-//                    finish();
-//                    startActivity(intent);
-                }
-                dialog.dismiss();
-            }
-        });
-
-        AlertDialog mDialog = mBuilder.create();
-        mDialog.show();
-    }
-
-    private void setLocale(String lang){
-        Locale locale = new Locale(lang);
-        Locale.setDefault(locale);
-        Configuration config = new Configuration();
-        config.setLocale(locale);
-        getActivity().getBaseContext().getResources().updateConfiguration(config,getActivity().getBaseContext().getResources().getDisplayMetrics());
-
-
-        SharedPreferences.Editor editor = this.getActivity().getSharedPreferences("Settings",MODE_PRIVATE).edit();
-        editor.putString("My_Language",lang);
-        editor.apply();
     }
 
 
 
-    ;
+
+
 }
