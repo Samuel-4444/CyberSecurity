3c917f1cdbb9157ac420887f79e37ce5bbf06542
Ace <aceshyjan@gmail.com>
2023-06-15 08:47:17 +0100

    Add onclicklistener

--- app/src/main/java/com/example/cybersecurityapp/menufrag/MenuHomeFragment.java
+++ app/src/main/java/com/example/cybersecurityapp/menufrag/MenuHomeFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.cybersecurityapp.R;
 
@@ -15,6 +17,18 @@ public class MenuHomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_home, container, false);
+
+        View rootView  = inflater.inflate(R.layout.fragment_menu_home,container,false);
+        Button button = rootView.findViewById(R.id.startQuizButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TESTACTION","THIS IS A TEST");
+            }
+        });
+
+        return rootView;
     }
+
+
 }
